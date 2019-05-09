4b91aa798b586452979461fca3eb184942e90fe9
ClementINTI <clement.wasselet@laposte.net>
2019-05-09 15:47:10 +0200

    Clement utilisateur entities

--- src/main/java/com/adaming/entities/Utilisateur.java
+++ src/main/java/com/adaming/entities/Utilisateur.java
@@ -1,5 +1,101 @@
 package com.adaming.entities;
 
-public class Utilisateur {
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Utilisateur implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String nom;
+
+	private String prenom;
+	
+	@OneToMany(mappedBy="utilisateur")
+	private Set<Produit> produits = new HashSet<>();
+	
+	
+	public Utilisateur(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	
+
+	public Utilisateur() {
+		super();
+	}
+
+
+
+	public Utilisateur(String nom, String prenom, Set<Produit> produits) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.produits = produits;
+	}
+
+	public Utilisateur(Long id, String nom, String prenom, Set<Produit> produits) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.produits = produits;
+	}
+
+	public Set<Produit> getProduits() {
+		return produits;
+	}
+
+	public void setProduits(Set<Produit> produits) {
+		this.produits = produits;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Utilisateur(Long id, String nom, String prenom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	
 }

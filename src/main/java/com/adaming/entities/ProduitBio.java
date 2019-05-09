5921c0e3f923992502ce90b35aa903f185a991d3
AlexandreINTI <al.poteau@outlook.fr>
2019-05-09 15:50:25 +0200

    MAJ produitBio

--- src/main/java/com/adaming/entities/ProduitBio.java
+++ src/main/java/com/adaming/entities/ProduitBio.java
@@ -1,5 +1,44 @@
 package com.adaming.entities;
 
-public class ProduitBio {
+import java.io.Serializable;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("ProduitBio")
+public class ProduitBio extends Produit implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	private String origine;
+
+	public ProduitBio() {
+		super();
+	}
+
+	public ProduitBio(String origine) {
+		super();
+		this.origine = origine;
+	}
+
+	public String getOrigine() {
+		return origine;
+	}
+
+	public void setOrigine(String origine) {
+		this.origine = origine;
+	}
+
+	@Override
+	public String toString() {
+		return "ProduitBio [origine=" + origine + "]";
+	}
+	
+	
 
 }
--- src/main/java/com/adaming/entities/services/IProduitAlimentaireService.java
+++ src/main/java/com/adaming/entities/services/IProduitAlimentaireService.java
@@ -1,5 +1,13 @@
 package com.adaming.entities.services;
 
+import java.util.List;
+
+import com.adaming.entities.ProduitAlimentaire;
+
 public interface IProduitAlimentaireService {
+	
+	public List<ProduitAlimentaire> findAll();
+	public ProduitAlimentaire findOne(ProduitAlimentaire prodAlim);
+	
 
 }

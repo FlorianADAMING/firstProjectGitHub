7f816f144816b3d9765d65884c03d9a79302464a
herve36 <romain.v@orange.fr>
2019-05-09 16:40:31 +0200

    classe utilisateur Romain

--- src/main/java/com/adaming/entities/Utilisateur.java
+++ src/main/java/com/adaming/entities/Utilisateur.java
@@ -1,6 +1,9 @@
 package com.adaming.entities;
 
+<<<<<<< Updated upstream
 import java.io.Serializable;
+=======
+>>>>>>> Stashed changes
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+<<<<<<< Updated upstream
 
 @Entity
 public class Utilisateur implements Serializable{
@@ -89,5 +93,67 @@ public class Utilisateur implements Serializable{
 		return "Utilisateur [idUtilisateur=" + idUtilisateur + ", nom=" + nom + ", prenom=" + prenom + "]";
 	}	
 	
+=======
+@Entity
+public class Utilisateur {
+>>>>>>> Stashed changes
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUtilisateur;
+	private String nom;
+	private String prenom;
+
+	@OneToMany(mappedBy = "utilisateur")
+	private Set<Produit> produits = new HashSet<Produit>();
+
+	public Utilisateur() {
+		super();
+	}
+
+	public Utilisateur(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	public Utilisateur(Long idUtilisateur, String nom, String prenom) {
+		super();
+		this.idUtilisateur = idUtilisateur;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	public Long getIdUtilisateur() {
+		return idUtilisateur;
+	}
+
+	public void setIdUtilisateur(Long idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Set<Produit> getProduits() {
+		return produits;
+	}
+
+	public void setProduits(Set<Produit> produits) {
+		this.produits = produits;
+	}
+	
 }

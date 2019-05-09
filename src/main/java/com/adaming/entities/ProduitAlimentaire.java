c987f8555f134a566e296ced9919188095ef25c8
PAelMagnifico <oftree99f@gmail.com>
2019-05-09 16:37:13 +0200

    Produit alimentaire de PA

--- src/main/java/com/adaming/entities/ProduitAlimentaire.java
+++ src/main/java/com/adaming/entities/ProduitAlimentaire.java
@@ -1,5 +1,40 @@
 package com.adaming.entities;
 
-public class ProduitAlimentaire {
+import java.io.Serializable;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="p_alimentaire")
+@DiscriminatorValue(value="alimentaire")
+public class ProduitAlimentaire extends Produit implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String nature;
+	public ProduitAlimentaire() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public ProduitAlimentaire(String nature) {
+		super();
+		this.nature = nature;
+	}
+	public String getNature() {
+		return nature;
+	}
+	public void setNature(String nature) {
+		this.nature = nature;
+	}
+	
 
 }
